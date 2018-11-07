0370314eb4c08370a24a885608fb5ebdf4c2650b
manvidas <manvidas@NM-16010-PC>
2018-11-07 13:53:16 +0000

    Added AccountController, Login view and Register view

---
+++ TeamReflection/TeamReflection/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace TeamReflection.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
