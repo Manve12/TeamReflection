61eb8778f934e16ff6fac0920400db3ade170b30
manvidas <manvidas@NM-16010-PC>
2018-11-07 13:16:14 +0000

    Added commented code for later use when calling stored procedures

--- TeamReflection/TeamReflection/Controllers/HomeController.cs
+++ TeamReflection/TeamReflection/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TeamReflection.Data;
 
 namespace TeamReflection.Controllers
 {
@@ -10,6 +13,25 @@ namespace TeamReflection.Controllers
     {
         public ActionResult Index()
         {
+
+            ///CODE BELOW IS FOR CALLING A PROCEDURE TO THE DATABASE AND STORING IT INSIDE OF DATATABLE
+
+            //SqlCommand cmd = DbConnect.ConnectionDatabase.CreateCommand();
+            //cmd.CommandType = CommandType.StoredProcedure;
+            //cmd.CommandText = "sp_TestProcedure";
+
+            //cmd.CommandTimeout = DbConnect.ConnectionTimeout;
+
+            //SqlDataAdapter da = new SqlDataAdapter(cmd);
+            //DataTable data = new DataTable();
+
+            //DbConnect.OpenConnection();
+
+            //da.Fill(data);
+
+            //DbConnect.CloseConnection();
+
+
             return View();
         }
 
