f59584f807d024939c0a710699a18a4749b78681
manvidas <manvidas@NM-16010-PC>
2018-11-07 13:40:31 +0000

    Updated DatabaseCall class to add ability to retrieve DataTable from the database depending on the procedure

--- TeamReflection/TeamReflection/Data/DatabaseCall.cs
+++ TeamReflection/TeamReflection/Data/DatabaseCall.cs
@@ -1,12 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
+using TeamReflection.Enum;
 
 namespace TeamReflection.Data
 {
     public static class DatabaseCall
     {
+        public static DataTable Get(StoredProcedures.Procedures procedure)
+        {
+            string procedureName = GetProcedureName(procedure);
 
+            if (procedureName == null)
+            {
+                return null;
+            }
+
+            SqlCommand cmd = DbConnect.ConnectionDatabase.CreateCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = procedureName;
+
+            cmd.CommandTimeout = DbConnect.ConnectionTimeout;
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable data = new DataTable();
+
+            DbConnect.OpenConnection();
+
+            da.Fill(data);
+
+            DbConnect.CloseConnection();
+
+            return data;
+        }
+
+        private static string GetProcedureName(StoredProcedures.Procedures procedure)
+        {
+            foreach (var item in StoredProcedures.StoredProcedure)
+            {
+                if (item.Key == procedure)
+                {
+                    return item.Value;
+                }
+            }
+            return null;
+        }
     }
 }
