0e5901ecdd4b418047d20f0bc88bd6d9a46a98ec
manvidas <manvidas@NM-16010-PC>
2018-11-07 13:30:14 +0000

    Added StoreProcedures class

---
+++ TeamReflection/TeamReflection/Enum/StoredProcedures.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace TeamReflection.Enum
+{
+    public static class StoredProcedures
+    {
+        public enum Procedures
+        {
+
+        }
+
+        public static Dictionary<Procedures, string> StoredProcedure = new Dictionary<Procedures, string>()
+        {
+            
+        }; 
+    }
+}
