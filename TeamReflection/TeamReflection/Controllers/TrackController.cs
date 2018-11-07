d2318c6e0936bced5ccdd11eca936b8a9122db3d
manvidas <manvidas@NM-16010-PC>
2018-11-07 13:52:05 +0000

    Added TrackController and Track view

---
+++ TeamReflection/TeamReflection/Controllers/TrackController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace TeamReflection.Controllers
+{
+    public class TrackController : Controller
+    {
+        public ActionResult Track()
+        {
+            return View();
+        }
+    }
+}
