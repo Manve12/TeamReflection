17f005af5dcbd21413fa554656f764cf408e3f6a
manvidas <manvidas@NM-16010-PC>
2018-11-15 10:23:18 +0000

    Updated AccountModel to have ConfirmPassword field

--- TeamReflection/TeamReflection/Models/AccountModel.cs
+++ TeamReflection/TeamReflection/Models/AccountModel.cs
@@ -10,6 +10,7 @@ namespace TeamReflection.Models
         public string Username { get; set; }
         public string Fullname { get; set; }
         public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string BillingFirstLine { get; set; }
