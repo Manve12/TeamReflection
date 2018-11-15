75020ca17776fa5c0243223b5711e9e0c529ee9b
manvidas <manvidas@NM-16010-PC>
2018-11-15 10:22:41 +0000

    Added AccountModel

---
+++ TeamReflection/TeamReflection/Models/AccountModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TeamReflection.Models
+{
+    public class AccountModel
+    {
+        public string Username { get; set; }
+        public string Fullname { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BillingFirstLine { get; set; }
+        public string BillingPostCode { get; set; }
+        public string BillingCounty { get; set; }
+    }
+}
